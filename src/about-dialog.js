42deea89ba6fff62a7295f4b257af2e5f4b6d823
Joseph Mawa <186199059+josephmawa@users.noreply.github.com>
2025-01-14 12:15:03 +0300

    chore: bump to v0.1.4

--- src/about-dialog.js
+++ src/about-dialog.js
@@ -13,7 +13,8 @@ const aboutParams = {
   copyright: "Copyright © 2024 Joseph Mawa",
   // The opening and closing backticks shouldn't be on separate lines
   translator_credits: `Sabri Ünal
-  volkov`,
+  volkov
+  John Peter Sa`,
   website: "https://github.com/josephmawa/Bella",
   issue_url: "https://github.com/josephmawa/Bella/issues",
   support_url: "https://github.com/josephmawa/Bella/issues",
